70b55585faa6a3e4e0718d49121ddd2604df02a7
Hendri Setiawan <hendri450450450@gmail.com>
2023-08-30 21:12:59 +0700

    menyelesaikan navbar dan membuat content halaman index page

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -6,32 +6,32 @@ import { RxCross1 } from 'react-icons/rx';
 
 
 const Header = () => {
-
+   
   const [open, setOpen] = useState()
- 
+
   let links = [
-    {name:"Home", link:"#home"},
-    {name:"About", link:"#about"},
-    {name:"Projects", link:"#projects"},
-  ]
+                {name:"HOME", link:"#home"},
+                {name:"ABOUT", link:"#about"},
+                {name:"PROJECTS", link:"#projects"},
+              ]
 
   return (
-    <div className='fixed top-0 left-0 w-full'>
-      <div className="md:flex items-center justify-between py-4 md:px-10 px-7">
-        <div>
-          <a className='text-3xl font-semibold' href="#home">Hendri Setiawan</a>
-          <div onClick={() => setOpen(!open)} className={`md:hidden border-b transition duration-500 ${open ? 'top-20 opacity-100  border-gray-300 py-3' : 'top-[-490px]'} opacity-0`}></div>
+    <div className={`fixed left-0 top-0 md:w-full w-[700px] ${open ? 'h-[200px]' : 'h-[75px]'} bg-black`}>
+      <div className='md:flex items-center justify-between py-4 md:px-10 px-7'>
+        <div className='md:flex'>
+          <a href="#home" className="text-3xl font-semibold transition duration-500 hover:text-slate-500">Hendri Setiawan</a>
+          <div className={`transition duration-500 ${open ? 'top-20 opacity-100 border-b border-gray-300 mt-[15px]' : 'top-[-490px]'} opacity-0`}></div>
         </div>
-        <div onClick={() => setOpen(!open)} className='md:hidden flex absolute text-3xl top-[23px] right-[25px] transition duration-500'>
+        <div onClick={() => setOpen(!open)} className='md:hidden text-3xl absolute top-5 right-5 transition duration-500 cursor-pointer hover:text-slate-500'>
           {
             open ? <RxCross1 />
                  : <BiMenu />
           }
         </div>
-        <ul className='md:flex md:mt-0 mt-[20px] items-center'>
+        <ul className='md:flex items-center'>
           {
             links.map((link) => (
-              <li className={`md:ml-8 md:my-0 my-5  text-xl transition duration-500 ${open ? 'top-20 opacity-100' : 'top-[-490px]'} opacity-0 md:opacity-100`} key="link.name"><a href={link.link}>{link.name}</a></li>
+              <li className={`hover:text-slate-500 md:ml-8 md:my-0 my-4 transition duration-500 ${open ? 'top-20 opacity-100' : 'top-[-490px]'} md:opacity-100 opacity-0`} key={link.name}><a href={link.link}>{link.name}</a></li>
             ))
           }
         </ul>
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
 const Home = () => {
   return (
-    <div></div>
+    <div className="md:px-10 px-7 py-4 mt-[50px]">
+      <div className="flex items-center justify-between">
+        <div className="flex">asd</div>
+        <div className="flex">asd</div>
+      </div>
+    </div>
   )
 }
 
