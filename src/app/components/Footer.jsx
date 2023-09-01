121e14cf175bdec9eb805f1c0fd9c430ccbc30cf
Hendri Setiawan <hendri450450450@gmail.com>
2023-09-01 19:07:02 +0700

    menambahkan penjelasan dari projects

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='text-center text-xl mb-[10px]'>
+    <footer className='text-center text-xl mb-[10px] mt-[400px]'>
       &copy; Copyright Hendri Setiawan2023  
     </footer>
   )
--- src/app/content/Projects.js
+++ src/app/content/Projects.js
@@ -14,6 +14,8 @@ const Projects = () => {
           height={700}
           alt="profile"
          />
+         <h2 className="ml-[150px] text-3xl font-semibold">Ecommerce</h2>
+         <p className='text-xl ml-[20px] mt-[20px]'>dibuat dengan Nuxt JS dan Tailwind CSS dan sudah terkoneksi dengan Rest Api. Rest API Mongo DB</p>
         </div>
         <div>
          <Image
@@ -22,6 +24,8 @@ const Projects = () => {
           height={700}
           alt="profile"
          />
+         <h2 className="ml-[150px] text-3xl font-semibold">Login System</h2>
+         <p className='text-xl ml-[20px] mt-[20px]'>dibuat dengan Next JS dan Tailwind CSS dan sudah terkoneksi dengan Rest Api. Rest API MYSQL</p>
         </div>
         <div>
          <Image
@@ -30,6 +34,8 @@ const Projects = () => {
           height={700}
           alt="profile"
          />
+         <h2 className="ml-[150px] text-3xl font-semibold">Search Engine</h2>
+         <p className='text-xl ml-[20px] mt-[20px]'>dibuat dengan Nuxt JS dan Tailwind CSS yang terhubung dengan library search engine</p>
         </div>
       </div>
     </div>
